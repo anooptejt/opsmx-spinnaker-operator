4d4516b4326f34ccdfe36f292f0fa675adb0eb7b
Funs <fozzielumpkins@gmail.com>
2019-07-27 12:19:17 -0700

    working clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -x
 
 ns="operators"
-grep="awk '{ print \$1 }' | grep -v NAME"
 kubectl delete crds/opsmxspinnakeroperators.charts.helm.k8s.io
-kubectl -n operators delete deployments \
-    $(kubectl get deployments -n $ns | $grep)
-svcs=$(kubectl get svc -n $ns | $grep)
+deployment= $(kubectl get deployments -n $ns | grep -v NAME | awk '{ print $1 }')
+if [ "$deployments" != "" ]; then
+    kubectl -n operators delete deployments $deployments
+fi
+svcs=$(kubectl get svc -n $ns | grep -v NAME | awk '{ print $1 }')
 for svc in $svcs; do 
-    kubectl -n operators delete svc $svc & 
+    kubectl -n operators delete svc $svc
 done
