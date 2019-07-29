7fae6b357ab8d1121922deff98ca14634293365c
Funs <fozzielumpkins@gmail.com>
2019-07-29 16:43:52 -0700

    meh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,5 @@ kubectl create -f ../deploy/service_account.yaml -n operators
 kubectl create -f ../deploy/role.yaml -n operators
 kubectl create -f ../deploy/role_binding.yaml -n operators
 kubectl create -f ../deploy/operator.yaml -n operators
-kubectl create -f ../deploy/crds/charts_v1alpha1_opsmxspinnakeroperator_cr.yaml -n operators
+# kubectl create -f ../deploy/crds/charts_v1alpha1_opsmxspinnakeroperator_cr.yaml -n operators
+kubectl create -f spinnaker.yaml -n operators
