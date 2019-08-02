ec435d206992616c9479deb62f51ec0c2f83a397
Funs <fozzielumpkins@gmail.com>
2019-08-02 13:58:07 -0700

    moving to os version..

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -19,5 +19,5 @@ done
 kubectl -n $ns delete -f  "../deploy/service_account.yaml"
 kubectl -n $ns delete -f  "../deploy/role.yaml"
 kubectl -n $ns delete -f  "../deploy/role_binding.yaml"
-minikube ssh "docker rmi zappo/ubi8-operator-halyard zappo/spinnaker-operator"
+miniskube ssh "docker rmi zappo/ubi8-operator-halyard zappo/spinnaker-operator"
 
