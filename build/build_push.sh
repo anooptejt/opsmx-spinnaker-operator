a8c8fef801b739494b497f88a430d4d635bb09e3
Funs <fozzielumpkins@gmail.com>
2019-08-13 17:29:25 -0700

    moving to named version

--- build/build_push.sh
+++ build/build_push.sh
@@ -2,4 +2,6 @@
 #
 cd ..
 docker build -t zappo/spinnaker-operator:latest -f build/Dockerfile .
+docker build -t zappo/spinnaker-operator:1.15.1-1 -f build/Dockerfile .
 docker push zappo/spinnaker-operator:latest
+docker push zappo/spinnaker-operator:1.15.1-1
