a9e26b1e6ce151d6cade1e7db787496407e33bc7
Funs <fozzielumpkins@gmail.com>
2019-09-24 13:00:02 -0700

    cleaning up clean, mapping oc+kubectl and using type correctly

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -8,14 +8,14 @@ if [ "$type" != "minikube" -a "$type" != "minishift" -a "$type" != "minispin" ];
   echo "$0: (minikube|minishit|minispin)"
   exit 1
 elif [ "$type" == "minikube" ]; then
-  type="kubectl"
+  kcmd="kubectl"
   mini="minikube"
 elif [ "$type" == "minishift" ]; then
-  type="oc"
+  kcmd="oc"
   mini="minishift"
 # in minispin, minikube runs under root, required for bare-metal deployment.
 elif [ "$type" == "minispin" ]; then
-  type="kubectl"
+  kcmd="kubectl"
   mini="sudo minikube"
 fi
 
@@ -24,24 +24,24 @@ if [ "$?" == "0" ]; then
     # patch so delete gets easy...
     # kubectl delete crds/$ncrd &
     # kubectl patch crds/$ncrd -p '{"metadata":{"finalizers":[]}}' --type=merge
-    kubectl delete crds/$ncrd
+    $kcmd delete crds/$ncrd
 
 fi
 deployments=$(kubectl get deployments -n $ns | grep -v NAME | awk '{ print $1 }')
 if [ "$deployments" != "" ]; then
-    kubectl -n $ns delete deployments $deployments
+    $kcmd -n $ns delete deployments $deployments
 fi
 svcs=$(kubectl get svc -n $ns | grep -v NAME | awk '{ print $1 }')
 for svc in $svcs; do 
-    kubectl -n $ns delete svc $svc
+    $kcmd -n $ns delete svc $svc
 done
 
-kubectl -n $ns delete -f  "../deploy/service_account.yaml"
-kubectl -n $ns delete -f  "../deploy/role.yaml"
-kubectl -n $ns delete -f  "../deploy/role_binding.yaml"
+$kcmd -n $ns delete -f  "../deploy/service_account.yaml"
+$kcmd -n $ns delete -f  "../deploy/role.yaml"
+$kcmd -n $ns delete -f  "../deploy/role_binding.yaml"
 TRUE=true
 while $TRUE; do
-  count=$(kubectl -n $ns get pods | grep pod | wc -l)
+  count=$($kcmd -n $ns get pods | grep pod | wc -l)
   if [ "$count" == "0" ];then
     echo "Pods are gone"
     TRUE=false
