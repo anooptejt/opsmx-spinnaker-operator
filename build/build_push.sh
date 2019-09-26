141841a5393249a0f2bcf98fc17e907e3dbb79f4
Funs <fozzielumpkins@gmail.com>
2019-09-26 09:16:48 -0700

    valid reference to gate ingress instead of ingress

--- build/build_push.sh
+++ build/build_push.sh
@@ -2,6 +2,6 @@
 #
 cd ..
 docker build -t zappo/spinnaker-operator:latest -f build/Dockerfile .
-docker build -t zappo/spinnaker-operator:1.15.1-4 -f build/Dockerfile .
+docker build -t zappo/spinnaker-operator:1.15.1-5 -f build/Dockerfile .
 docker push zappo/spinnaker-operator:latest
-docker push zappo/spinnaker-operator:1.15.1-4
+docker push zappo/spinnaker-operator:1.15.1-5
