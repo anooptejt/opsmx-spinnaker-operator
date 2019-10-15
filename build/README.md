1d712ba7c85204e24155b31464a8103ca5fe73e4
Funs <fozzielumpkins@gmail.com>
2019-10-15 13:00:47 -0700

    readme update, options, usage and move files around

--- build/README.md
+++ build/README.md
@@ -1,7 +1,14 @@
-Updates
-=======
-After changes run:
-  bash update_tags.sh 1.15.1-2 1.15.1-3
-  bash build_push
+This directory contains the Dockerfile, and a push script for the Operator Container
 
-to push a newer version of the image upstream
+# build_push.sh
+Builds the container and pushes it to its specific Red Hat and Docker registry.
+
+Usage: ./build_push [OPTION...]
+-o|--operator-name=STRING Name of the operator container
+-k|--keys-location=STRING Location of the keys
+-r|--push-redhat          Push to the Red Hat registry
+-d|--push-docker          Push to the Docker registry
+-V|--version=VERSION      Version to tag the image with
+-v|--verbose              Does nothing
+-h|--help                 This usage
+e.g., ./build_push -o spinnaker-operator -k $HOME/.keys -r -p -V 1.16.0-1
--- build/build_push.sh
+++ build/build_push.sh
@@ -2,10 +2,63 @@
 #
 # Builds the spinnaker operator and pushes it to selected repos
 #
-name="spinnaker-operator"
-ver="1.16.0-1"
+while [ "$1" != "" ]; do
+    case $1 in
+    -o|--operator-name)
+        shift
+        name=$1
+        ;;
+    -k|--keys-location)
+        shift
+        keys=$1
+        ;;
+    -r|--push-redhat)
+        pushRh=1
+        ;;
+    -d|--push-docker)
+        pushDock=1
+        ;;
+    -V|--version)
+        shift
+        ver=$1
+        ;;
+    -h|--help)
+        $0
+        exit 1
+        ;;
+    *)
+        $0
+        exit 1
+    esac
+    shift
+done
+
+usage() {
+  echo "Usage: $0 [OPTION...]
+  -o|--operator-name=STRING Name of the operator container
+  -k|--keys-location=STRING Location of the keys
+  -r|--push-redhat          Push to the Red Hat registry
+  -d|--push-docker          Push to the Docker registry
+  -V|--version=VERSION      Version to tag the image with
+  -v|--verbose              Does nothing
+  -h|--help                 This usage
+  e.g., $0 -o spinnaker-operator -k $HOME/.keys -r -p -V 1.16.0-1
+"
+}
+
+pushRemote() {
+    repo=$1
+    user=$2
+    credsFile=$3
+    image=$4
+
+    cat $credsFile | docker login --username $user --password-stdin $repo
+    docker push $image
+}
+
+name=${name:-spinnaker-operator}
+keys=${keys:-$HOME/.keys}
 dfile="build/Dockerfile"
-keys=$HOME/.keys
 repo="devopsmx"
 latest="${repo}/${name}:latest"
 image="${repo}/${name}:${ver}"
@@ -23,26 +76,24 @@ rhCredsFile="${keys}/pidkey-${rhPid}"
 gcrReg="gcr.io"
 gcrRepo="opsmx-images"
 gcrImage="${gcrReg}/${gcrExt}/${name}:${ver}"
-gcrUser="opsmxadmn@gmail.com"
+gcrUser="opsmxadm@gmail.com"
 gcrCredsFile="${keys}/gcr.auth"
 
-pushRemote() {
-  repo=$1
-  user=$2
-  credsFile=$3
-  image=$4
-
-  cat $credsFile | docker login --username $user --password-stdin $repo
-  docker push $image
-}
+if [ -z "$ver" ];then
+    usage
+    exit 1
+fi
 
 cd ..
 docker build -t $latest -f ${dfile} .
 docker build -t $image -f ${dfile} .
-docker tag $latest $rhImage
 # docker tag $latest $gcrImage
-
-pushRemote $dockerReg $dockerUser $dockerCredsFile $image
-pushRemote $dockerReg $dockerUser $dockerCredsFile $latest
-# pushRemote $rhReg $rhUser $rhCredsFile $rhImage
+if [ "$pushDock" == "1" ];then
+    pushRemote $dockerReg $dockerUser $dockerCredsFile $image
+    pushRemote $dockerReg $dockerUser $dockerCredsFile $latest
+fi
+if [ "$pushRh" == "1" ]; then
+    docker tag $latest $rhImage
+    pushRemote $rhReg $rhUser $rhCredsFile $rhImage
+fi
 # pushRemote $gcrRepo $gcrUser $gcrCredsFile $gcrImage
